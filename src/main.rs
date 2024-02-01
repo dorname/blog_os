b1f6227566b90bdcffa95b0e87d0e4e990b826fb
dorname <lgqfighting@163.com>
2024-02-02 00:35:21 +0800

    完成测试模块

--- src/main.rs
+++ src/main.rs
@@ -2,84 +2,33 @@
 #![no_std] // 不链接 Rust 标准库
 #![no_main] // 禁用所有 Rust 层级的入口点
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(blog_os::test_runner)]
 
 
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
 use core::panic::PanicInfo;
-
+use blog_os::println;
 /// 这个函数将在 panic 时被调用
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("my_panic:{}", _info);
     loop {}
 }
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> !{
+    blog_os::test_panic_handler(info);
+}
 
-// static HELLO: &[u8] = b"Hello World!";
-
-// #[no_mangle]
-// pub extern "C" fn _start() -> ! {
-//     let vga_buffer = 0xb8000 as *mut u8;
-
-//     for (i, &byte) in HELLO.iter().enumerate() {
-//         unsafe {
-//             *vga_buffer.offset(i as isize * 2) = byte;
-//             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-//         }
-//     }
-
-//     loop {}
-// }
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // use core::fmt::Write;
-    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    // vga_buffer::WRITER.lock().write_str("Hello again again").unwrap();
-    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+
     println!("Hello World{}", "!");
-    // panic!("This is a panic message");
     
     #[cfg(test)]
     test_main();
     
     loop {}
-}
-
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-
-#[test_case]
-fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    serial_println!("[ok]");
-}
-
-#[derive(Debug,Clone, Copy,PartialEq,Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11
-}
-
-pub fn exit_qemu(exit_code:QemuExitCode){
-    use x86_64::instructions::port::Port;
-
-    unsafe{
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
 }
